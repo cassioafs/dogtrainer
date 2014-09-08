029f29af17e64e13655dba59a083e91713e7fc9f
Cassio Alves <cassiocafs@gmail.com>
2014-09-08 10:06:36 -0300

    #1 commit

---
+++ src/main/java/br/com/dog/trainer/controller/PessoaController.java
@@ -0,0 +1,11 @@
+package br.com.dog.trainer.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+public class PessoaController {
+
+	@Path("/pessoa/nova")
+	public void nova(){}
+}
