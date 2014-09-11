fed7ade7205346ab9a173b67e50547b2d96d57da
Cassio Alves <cassiocafs@gmail.com>
2014-09-11 15:44:50 -0300

    criando sitemesh

--- src/main/webapp/js/app.js
+++
@@ -1,9 +0,0 @@
-$.validator.setDefaults({
-	highlight: function(label) {
-	    $(label).closest('.control-group').addClass('error');
-	},
-	success: function(label) {
-		$(label).closest('.control-group').removeClass('error');
-		$(label).remove();
-	}
-});
